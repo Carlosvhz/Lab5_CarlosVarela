7bab867c0bf2c95163fba9b766de8b91d994216d
carlosvhz <carlosvhz@outlook.com>
2017-11-17 16:07:04 -0600

    Commit 5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 #include "persona.h"
-
+#include <vector>
 
 using namespace std;
 
-int main(){
+//Prototipo
+void guardarArchivo(persona*);
+void agregar();
+void set
 
+
+int main(){
+  char resp;
+  int op;
+  vector<persona*> personas;
+  do{
+    cout<<"====== Lab#5 ======"<<endl;
+    cout<<"1. Agregar"<<endl
+        <<"2. Listar"<<endl
+        <<"3. Eliminar"<<endl
+        <<"4. Guardar en archivo"<<endl
+        <<"Ingrese opcion: ";
+    cin>>op;
+    /*switch(op){
+      case 1:
+    }*/
+    cout<<"¿Continuar?[s]: ";
+    cin>>resp;
+  }while(resp=='s');
+  return 0;
 }
